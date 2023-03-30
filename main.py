1df13926de834e6c6db802d243d022eaf1fc3451
Arthur Stolyarchuk <50176561+EDM1337@users.noreply.github.com>
2023-03-30 15:35:09 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+filename = "population.txt"
+
+def fileRead(filename):
+    country_populations = {}
+
+    with open(filename, 'r') as file:
+        for line in file:
+            country, year, population = line.strip().split(", ")
+            population = int(population)
+
+            if country in country_populations:
+                country_populations[country].append((year, population))
+            else:
+                country_populations[country] = [(year, population)]
+
+    return country_populations
+
+def printPopulation(country_populations):
+    for country, populations in country_populations.items():
+        print(country + ":")
+        for i in range(len(populations) - 1):
+            year1, pop1 = populations[i]
+            year2, pop2 = populations[i + 1]
+            pop_change = pop2 - pop1
+            print(year1 + " -> " + year2 + ": " + str(pop_change))
+
+
+if __name__ == "__main__":
+    filename = "population.txt"
+    country_populations = fileRead(filename)
+    printPopulation(country_populations)
+
+
---
+++ test_main.py
@@ -0,0 +1,32 @@
+import pytest
+from main import fileRead
+from main import printPopulation
+
+def test_fileRead():
+    # create a test file with some data
+    with open('test_data.txt', 'w') as file:
+        file.write("USA, 2020, 330000000\n")
+        file.write("USA, 2021, 335000000\n")
+        file.write("India, 2020, 1380000000\n")
+        file.write("India, 2021, 1390000000\n")
+
+    # call the fileRead() function with the test file
+    country_populations = fileRead('test_data.txt')
+
+    # check if the output is as expected
+    assert country_populations == {
+        'USA': [('2020', 330000000), ('2021', 335000000)],
+        'India': [('2020', 1380000000), ('2021', 1390000000)]
+    }
+
+@pytest.fixture
+def country_populations():
+    return {
+        'USA': [('2020', 330000000), ('2021', 335000000)],
+        'India': [('2020', 1380000000), ('2021', 1390000000)]
+    }
+
+def test_printPopulation(capfd, country_populations):
+    printPopulation(country_populations)
+    out, err = capfd.readouterr()
+    assert out == "USA:\n2020 -> 2021: 5000000\nIndia:\n2020 -> 2021: 10000000\n"
